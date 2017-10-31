bf18a5fad370f4a16bbc3f6c658e383957880e28
Piotr Hendzel <phinsectoman@gmail.com>
2017-10-31 08:50:51 +0100

    Initial commit #1 Add main class Add pom.xml Configure properly version of spring boot

---
+++ src/main/java/xyz/javista/FoodDelivery.java
@@ -0,0 +1,13 @@
+package xyz.javista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class FoodDelivery {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FoodDelivery.class, args);
+    }
+
+}
