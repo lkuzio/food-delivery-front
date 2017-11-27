16f76e9d1d9820ff2872dd803add1273a4846a9a
lkuzio <lkuzio@gmail.com>
2017-11-27 11:38:07 +0100

    :sparkles: improvement for heroku deployment

--- Server.js
+++ Server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
+
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname + '/dist')));
+
+app.get('*', (req, res)=> {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8090);
