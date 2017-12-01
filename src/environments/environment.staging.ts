4ea4d59104fd961b1083b415a8daec28bf1443d9
lkuzio <lkuzio@gmail.com>
2017-12-01 14:58:21 +0100

    :recycle: CD changes

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: "",//'https://food-delivery-javista.herokuapp.com/',
+  apiEndpoint: 'https://food-delivery-javista.herokuapp.com/',
   envName: 'staging',
   clientId: "asdasdasd",
   clientSecret: "asdasdasd"
