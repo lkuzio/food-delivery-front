9c1ad0867e979fa511957c8ae5ef2fe502707a2e
Krzysztof Klonowski <klonek@10g.pl>
2017-12-14 10:38:07 +0100

    :bug: redirect fixed after token expiration

--- src/app/commons/ErrorInterceptor.ts
+++ src/app/commons/ErrorInterceptor.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http'
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
-import {Router} from "@angular/router";
+import {NavigationExtras, Router} from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -14,16 +14,21 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).do(event => {
     }, err => {
-      if (err instanceof HttpErrorResponse && err.status == 401) {
+      if (err instanceof HttpErrorResponse && err.status === 401) {
         localStorage.removeItem('token');
         localStorage.removeItem('user_id');
         localStorage.removeItem('user_login');
         localStorage.removeItem('user_email');
         localStorage.removeItem('user_name');
         localStorage.removeItem('refresh_token');
-        localStorage.removeItem("token_expires_in");
-        localStorage.removeItem("stayLoggedIn");
-        this.router.navigateByUrl('/login');
+        localStorage.removeItem('token_expires_in');
+        localStorage.removeItem('stayLoggedIn');
+        const navigationExtras: NavigationExtras = {
+          queryParams: {
+            returnUrl: this.router.url
+          }
+        };
+        this.router.navigate(['/login'], navigationExtras);
         location.reload();
       }
     });
